1b7b03c34b2332ba25f4759bfa5ec012c355b030
Dennis <adigwedennis@yahoo.com>
2022-07-20 22:04:38 +0100

    Projected Completed with saved instance state

--- app/src/main/java/com/denniscode/swooh/LeagueActivity.kt
+++ app/src/main/java/com/denniscode/swooh/LeagueActivity.kt
@@ -3,12 +3,16 @@ package com.denniscode.swooh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import android.widget.ToggleButton
 
 class LeagueActivity : AppCompatActivity() {
+    var person = Person(player = "", skills = "")
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_VALUE, person)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
@@ -16,8 +20,7 @@ class LeagueActivity : AppCompatActivity() {
         val womensBtn = findViewById<ToggleButton>(R.id.womensBtn)
         val coEdBtn = findViewById<ToggleButton>(R.id.coEdBtn)
         val nextSkillBtn : Button = findViewById(R.id.nextSkillBtn)
-        var person = Person(player = "", skills = "")
-        mensBtn.setOnClickListener(){
+        mensBtn.setOnClickListener{
             womensBtn.isChecked = false
             coEdBtn.isChecked = false
             person.player = "Mens"
@@ -32,7 +35,7 @@ class LeagueActivity : AppCompatActivity() {
             mensBtn.isChecked = false
             person.player = "Co-ed"
         }
-        nextSkillBtn.setOnClickListener() {
+        nextSkillBtn.setOnClickListener {
             if (person.player == "") {
                 Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
             } else {
@@ -41,5 +44,10 @@ class LeagueActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        person = savedInstanceState.getParcelable(EXTRA_VALUE)!!
     }
 }
